1c569d664f60c6fe10ba6220175b9e1ee1420a3c
Chinmay Shet <chishxd@gmail.com>
2025-11-14 00:23:50 +0530

    docs: Add comments for my goldfish ahh memory
    Learning from mistakes in earlier projects

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,40 @@
-import type { GithubCommit } from '$lib/types';
+/**
+ * @file Server-Side logic for StellarLog
+ *
+ * @description This file handles the data-loading stuff for our application
+ * It receives GH repo link via that form in client(named REPO)
+ * then I fetches the commit history and returns an object with all the commits and error(if any)
+ *
+ * I made this file to avoid CORS issues(it was happening previously, yeah)
+ */
 
-export const load = async ({ url }) => {
+import type { GithubCommit } from '$lib/types';
+import type { PageServerLoad } from './$types.js';
+
+/**
+ * SvelteKit's load function that fetches commit data based on URL query
+ * @param {object} event - The SvelteKit's implicit load event object
+ * @param {URL} event.url - Contains the URL of the current request, including search parameters.
+ * @returns {Promise<{commits: GithubCommit[], error: string | null}>}
+ */
+export const load: PageServerLoad = async ({
+	url
+}: {
+	url: URL;
+}): Promise<{ commits: GithubCommit[]; error: string | null }> => {
 	const repoUrl = url.searchParams.get('repo');
 
 	if (!repoUrl) {
-		return { commits: [] as GithubCommit[] };
+		return { commits: [] as GithubCommit[], error: null };
 	}
 
 	try {
+		// Parsing / Formatting the URL here.. IDK if I needed to comment that
 		const urlParts = repoUrl.split('/');
 		const owner = urlParts[urlParts.length - 2];
 		const repo = urlParts[urlParts.length - 1];
 
+		// Build url for API and fetch data
 		const apiUrl = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=100`;
 		const response = await fetch(apiUrl);
 		if (!response.ok) {
