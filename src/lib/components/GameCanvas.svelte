78bc6ee1a528c8ae5a5be6e2608cdb99562dd7da
Chinmay Shet <chishxd@gmail.com>
2025-11-15 00:48:35 +0530

    feat: render commits as stars

--- src/lib/components/GameCanvas.svelte
+++ src/lib/components/GameCanvas.svelte
@@ -14,17 +14,34 @@
 
 			// This is the only place I ASKED ai how can I declare a class which requires contents from dynamically imported stuff
 			GameScene = class extends PhaserDefault.Scene {
+				commitData: GithubCommit[];
+
 				constructor() {
 					super({ key: 'GameScene' });
+					this.commitData = [];
 				}
 
 				init(data: { commits: GithubCommit[] }) {
 					console.log('Phaser scene has been initialized with data.', data);
+					this.commitData = data.commits;
 				}
 
 				create() {
 					console.log('Phaser `create` method is running.');
 					this.cameras.main.setBackgroundColor('#000000');
+
+					// Rendering logic for stars reside below this comment
+					const startX = 50;
+					const spacingX = 15;
+					const y = 300;
+					const radius = 4;
+					const color = 0xffffff;
+
+					this.commitData.forEach((commit, index) => {
+						const x = startX + index * spacingX;
+
+						this.add.circle(x, y, radius, color);
+					});
 				}
 			};
 
